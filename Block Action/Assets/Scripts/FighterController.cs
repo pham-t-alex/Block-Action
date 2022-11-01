5666280210b3b7aaefc05a5e14a8182c145d971b
Alexander Pham <108191659+pham-t-alex@users.noreply.github.com>
2022-11-01 10:48:50 -0700

    placement and sprites
    -automatic player and enemy placement
    -tested with player sprites and enemy sprites

--- Block Action/Assets/Scripts/FighterController.cs
+++ Block Action/Assets/Scripts/FighterController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FighterController : MonoBehaviour
 {
+    public float spaceBetweenEnemies;
+    public float bottomOffset;
+    public float minRightOffset;
+
     private static FighterController _fighterController;
     public static FighterController fighterController
     {
@@ -30,6 +34,50 @@ public class FighterController : MonoBehaviour
 
     public static void PlaceFighters()
     {
+        PlacePlayer();
+        PlaceEnemies();
+    }
 
+    static void PlacePlayer()
+    {
+        float y = -1 * Camera.main.orthographicSize;
+        y += fighterController.bottomOffset;
+        SpriteRenderer playerSprite = Player.player.GetComponent<SpriteRenderer>();
+        y += playerSprite.bounds.size.y / 2;
+        Player.player.transform.position = new Vector3(-7, y, 0);
+    }
+
+    static void PlaceEnemies()
+    {
+        float y = -1 * Camera.main.orthographicSize;
+        y += fighterController.bottomOffset;
+        float totalWidth = 0;
+        for (int i = 0; i < Battle.b.enemies.Count; i++)
+        {
+            if (i > 0)
+            {
+                totalWidth += fighterController.spaceBetweenEnemies;
+            }
+            totalWidth += Battle.b.enemies[i].GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+        float rightOffset = 0;
+        if (totalWidth < (Camera.main.orthographicSize * Screen.width / Screen.height) - 2 * (fighterController.minRightOffset))
+        {
+            rightOffset = ((Camera.main.orthographicSize * Screen.width / Screen.height) - totalWidth) / 2;
+        } else
+        {
+            rightOffset = fighterController.minRightOffset;
+        }
+        float x = (Camera.main.orthographicSize * Screen.width / Screen.height) - rightOffset;
+        for (int i = Battle.b.enemies.Count - 1; i >= 0; i--)
+        {
+            SpriteRenderer spriteRenderer = Battle.b.enemies[i].GetComponent<SpriteRenderer>();
+            x -= spriteRenderer.bounds.size.x / 2;
+            y += spriteRenderer.bounds.size.y / 2;
+            Battle.b.enemies[i].transform.position = new Vector3(x, y, 0);
+            x -= spriteRenderer.bounds.size.x / 2;
+            y -= spriteRenderer.bounds.size.y / 2;
+            x -= fighterController.spaceBetweenEnemies;
+        }
     }
 }
--- Block Action/Assets/Scripts/Player.cs
+++ Block Action/Assets/Scripts/Player.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : Fighter
-{ 
-    public static Player player;
+{
+    private static Player _player;
+    public static Player player
+    {
+        get
+        {
+            if (_player == null)
+            {
+                _player = FindObjectOfType<Player>();
+            }
+            return _player;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -12,7 +23,6 @@ public class Player : Fighter
         health = 100; //might not be valid
         buff = 1.0;
         maxHealth = 100;
-        player = this;
         buffLeft = new List<BuffCounter>();
 
         Battle.b.fighters.Add(this);
