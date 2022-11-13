0846a522a5639bd480f17d556833fc41faa246f3
Alexander Pham <108191659+pham-t-alex@users.noreply.github.com>
2022-11-12 22:45:59 -0800

    midlevel damage effect

--- Block Action/Assets/Scripts/GimmickController.cs
+++ Block Action/Assets/Scripts/GimmickController.cs
@@ -40,24 +40,76 @@ public class GimmickController : MonoBehaviour
             for (int i = 0; i < gimmickController.midLevelEffects.Count; i++)
             {
                 string[] gimmickInfo = gimmickController.midLevelEffects[i].Split(" ");
-                if (gimmickInfo[0].Equals("Turn"))
+                if (gimmickInfo[0].Equals("turn"))
                 {
                     int turn = System.Convert.ToInt32(gimmickInfo[1]);
                     if (turn == Battle.b.turnNumber)
                     {
-                        if (gimmickInfo[2].Equals("Text"))
-                        {
-                            TextAsset t = (TextAsset) Resources.Load($"Levels/{gimmickInfo[3]}");
-                            gimmickController.text = t.text;
-                            Debug.Log(gimmickController.text);
-                            gimmickController.midLevelEffects.RemoveAt(i);
-                            i--;
-                        }
+                        ActivateMidLevelEffect(gimmickInfo);
+                        gimmickController.midLevelEffects.RemoveAt(i);
+                        i--;
+                    }
+                }
+                else if (gimmickInfo[0].Equals("wave"))
+                {
+                    int wave = System.Convert.ToInt32(gimmickInfo[1]);
+                    if (wave == FighterController.fighterController.wave)
+                    {
+                        ActivateMidLevelEffect(gimmickInfo);
+                        gimmickController.midLevelEffects.RemoveAt(i);
+                        i--;
                     }
                 }
             }
             Battle.b.bs = BattleState.PlayerGrid;
             return;
+        }
+    }
+
+    public static void ActivateMidLevelEffect(string[] gimmickInfo)
+    {
+        if (gimmickInfo[2].Equals("text"))
+        {
+            TextAsset t = (TextAsset)Resources.Load($"Levels/{gimmickInfo[3]}");
+            gimmickController.text = t.text;
+            Debug.Log(gimmickController.text);
+        }
+        else if (gimmickInfo[2].Equals("damage"))
+        {
+            int damage = System.Convert.ToInt32(gimmickInfo[4]);
+            if (gimmickInfo[3].Equals("player"))
+            {
+                if (!Player.player.dead)
+                {
+                    Player.player.health -= damage;
+                    if (Player.player.health <= 0)
+                    {
+                        Player.player.dead = true;
+                        Player.player.buff = 1;
+                        Player.player.buffLeft.Clear();
+                        Player.player.healthBar.gameObject.SetActive(false);
+                        Player.player.gameObject.SetActive(false);
+                    }
+                }
+            }
+            else if (gimmickInfo[3].Equals("enemies"))
+            {
+                foreach (Enemy e in Battle.b.enemies)
+                {
+                    if (!e.dead)
+                    {
+                        e.health -= damage;
+                        if (e.health <= 0)
+                        {
+                            e.dead = true;
+                            e.buff = 1;
+                            e.buffLeft.Clear();
+                            e.healthBar.gameObject.SetActive(false);
+                            e.gameObject.SetActive(false);
+                        }
+                    }
+                }
+            }
         }
     }
 }
