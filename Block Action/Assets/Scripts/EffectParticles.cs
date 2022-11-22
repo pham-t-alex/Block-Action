f32803d9e763b18efc60e54827f4a4c060d0bd50
pham-t-alex <pham.t.alex@gmail.com>
2022-11-21 20:53:33 -0800

    block usage particles

---
+++ Block Action/Assets/Scripts/EffectParticles.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectParticles : MonoBehaviour
+{
+    float timeLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            Destroy(this);
+        }
+    }
+}
