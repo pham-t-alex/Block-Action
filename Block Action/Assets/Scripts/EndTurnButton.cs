1d76da02eb277ca4dcbde2b54ffe3e703dbc13d4
pham-t-alex <pham.t.alex@gmail.com>
2022-11-25 15:35:16 -0800

    lantern end turn button

--- Block Action/Assets/Scripts/EndTurnButton.cs
+++ Block Action/Assets/Scripts/EndTurnButton.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndTurnButton : MonoBehaviour
-{ 
-
+{
+    public GameObject endTurnButton;
+    Image img;
+    public Sprite lantern;
+    public Sprite darkLantern;
     // Start is called before the first frame update
     void Start()
     {
-        
+        img = endTurnButton.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Battle.b.bs == BattleState.PlayerAction || Battle.b.bs == BattleState.EnemyAction)
+        {
+            img.sprite = darkLantern;
+        }
+        else
+        {
+            img.sprite = lantern;
+        }
     }
 
     public void OnMouseDown() {
